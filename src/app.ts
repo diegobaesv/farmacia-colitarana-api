ef94b7b43bc5ed9cb7bc3cd0b5f4dad5d6fc0145
DiegoB <diegobaesv@gmail.com>
2024-12-02 22:38:26 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import morgan from 'morgan';
 const app: Application = express();
 
 app.get('/saludar',(req: Request, res: Response)=>{
-    console.log('Hola estoy saludando');
     res.json({nombre:'Diego Fernando', apellido: 'Baes'});
 });
 
