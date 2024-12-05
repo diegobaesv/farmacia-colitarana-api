d002baab56e10427cef43867e280a57d2b77ae29
DiegoB <diegobaesv@gmail.com>
2024-12-04 22:37:50 -0500

    proveedor service

--- src/controllers/provedor.controller.ts
+++ src/controllers/provedor.controller.ts
@@ -1,22 +1,33 @@
 import { Request, Response } from 'express';
+import * as proveedorService from '../services/proveedor.service';
 
 export const insertarProveedor = async (req: Request, res: Response) => {
-    res.json({accion:'insertarProveedor'});
+    const proveedor = req.body;
+    const response = proveedorService.insertarProveedor(proveedor);
+    res.json(response);
 }
 
 export const listarProveedor = async (req: Request, res: Response) => {
-    res.json({accion:'listarProveedoor'});
+    const response = proveedorService.listarProveedor();
+    res.json(response);
 }
 
 export const obtenerProveedor = async (req: Request, res: Response) => {
-    res.json({accion:'obtenerProveedor'});
+    const { idProveedor } = req.params;
+    const response = proveedorService.obtenerProveedor(Number(idProveedor));
+    res.json(response);
 }
 
 export const actualizarProveedor = async (req: Request, res: Response) => {
-    res.json({accion:'actualizarProveedor'});
+    const { idProveedor } = req.params;
+    const proveedor = req.body;
+    const response = proveedorService.actualizarProveedor(Number(idProveedor),proveedor)
+    res.json(response);
 }
 
 export const darBajaProveedor = async (req: Request, res: Response) => {
-    res.json({accion:'darBajaProoveedor'});
+    const { idProveedor } = req.params;
+    const response = proveedorService.darBajaProveedor(Number(idProveedor));
+    res.json(response);
 }
 
