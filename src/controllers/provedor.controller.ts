77fc50a95bbfe95c71f66860cec3b2013c4a16ff
DiegoB <diegobaesv@gmail.com>
2024-12-04 22:02:41 -0500

    proveedor router controller

---
+++ src/controllers/provedor.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+
+export const insertarProveedor = async (req: Request, res: Response) => {
+    res.json({accion:'insertarProveedor'});
+}
+
+export const listarProveedor = async (req: Request, res: Response) => {
+    res.json({accion:'listarProveedoor'});
+}
+
+export const obtenerProveedor = async (req: Request, res: Response) => {
+    res.json({accion:'obtenerProveedor'});
+}
+
+export const actualizarProveedor = async (req: Request, res: Response) => {
+    res.json({accion:'actualizarProveedor'});
+}
+
+export const darBajaProveedor = async (req: Request, res: Response) => {
+    res.json({accion:'darBajaProoveedor'});
+}
+
