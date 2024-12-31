35969defba9e62771ddc1e594f0a3d668eb01cf5
DiegoB <diegobaesv@gmail.com>
2024-12-30 21:36:04 -0500

    schema proveedor

---
+++ src/validators/proveedor.schema.ts
@@ -0,0 +1,37 @@
+import Joi from "joi";
+
+export const insertarProveedorSchema = Joi.object({
+    razonSocial: Joi.string()
+                    .min(3)
+                    .max(200)
+                    .required(),
+    tipoDocumento: Joi.string()
+                    .min(1)
+                    .max(1)
+                    .valid('R','D','C')
+                    .required(),
+    numeroDocumento: Joi.string()
+                    .min(8)
+                    .max(15)
+                    .required()
+                    .pattern(new RegExp('^[0-9]{8,15}$')),
+    telefono: Joi.string()
+                .min(9)
+                .max(20)
+                .optional(),
+    correo: Joi.string()
+            .email()
+            .min(3)
+            .max(100)
+            .optional(),
+    direccion: Joi.string()
+                .min(3)
+                .max(500)
+                .optional()
+                
+    
+});
+
+export const actualizarProveedorSchema = Joi.object({
+
+});
